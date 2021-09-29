0ee0d126d84df8bd5c32ee8c4daa4b41467ed1b1
Maxime Vandegar <45870423+MaximeVandegar@users.noreply.github.com>
2021-09-29 13:08:39 +0200

    correct initialization to Inf

--- src/diffoptics/ray_tracing/RayTracing.py
+++ src/diffoptics/ray_tracing/RayTracing.py
@@ -15,7 +15,7 @@ def trace_rays(incident_rays: Rays, scene: Scene) -> Tuple[Rays, torch.Tensor, t
     :return:
     """
     device = incident_rays.device
-    t = torch.empty(incident_rays.origins.shape[0], device=device) + float('Inf')
+    t = torch.zeros(incident_rays.origins.shape[0], device=device) + float('Inf')
     outgoing_ray = Rays(torch.empty((incident_rays.origins.shape[0], 3),
                                     dtype=incident_rays.origins.dtype) + float('nan'),
                         torch.empty((incident_rays.directions.shape[0], 3),
