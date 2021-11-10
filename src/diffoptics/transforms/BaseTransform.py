8c5afe39749e60fe470ae0c51d9df6fbd774e409
Maxime Vandegar <maxvandegar@gmail.com>
2021-11-10 22:30:05 +0100

    renaming function

--- src/diffoptics/transforms/BaseTransform.py
+++ src/diffoptics/transforms/BaseTransform.py
@@ -14,7 +14,7 @@ class BaseTransform(abc.ABC):
                                         device=rays.device)), dim=1).unsqueeze(-1))[:, :3, 0]
         return Rays(new_o, new_d, luminosities=rays.luminosities, meta=rays.meta, device=rays.device)
 
-    def inverse_transform(self, rays: Rays):
+    def apply_inverse_transform(self, rays: Rays):
         new_o = torch.matmul(self.inverse_transform.type(rays.origins.dtype).to(rays.device),
                              torch.cat((rays.origins, torch.ones((rays.origins.shape[0], 1),
                                         device=rays.device)), dim=1).unsqueeze(-1))[:, :3, 0]
